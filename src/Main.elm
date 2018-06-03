4dbad0fb67563604ed1c9fc16c6c5a4e6c78b19d
Matthew King <mattking73@gmail.com>
2018-06-02 18:19:52 -0700

    Add basic todo/completed lists and buttons

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,131 @@
 module Main exposing (..)
 
-import Html exposing (Html, span, text)
-import Html.Attributes exposing (class)
+import Html exposing (Html, button, div, h6, input, span, text)
+import Html.Attributes exposing (class, value)
+import Html.Events exposing (onClick, onInput)
 
 
-main : Html a
 main =
-    span [ class "welcome-message" ] [ text "Hello, World" ]
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { todo : List String
+    , completed : List String
+    , currentStandupTask : String
+    }
+
+
+model : Model
+model =
+    { todo = []
+    , completed = []
+    , currentStandupTask = ""
+    }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ input [ onInput ChangeStandupTask, value model.currentStandupTask ] []
+        , button [ onClick Add ] [ text "Add" ]
+        , div []
+            [ h6 [] [ text "TODO" ]
+            , viewTodoTasks model.todo
+            ]
+        , div []
+            [ h6 [] [ text "Completed" ]
+            , viewCompletedTasks model.completed
+            ]
+        ]
+
+
+viewTodoTasks : List String -> Html Msg
+viewTodoTasks todoTasks =
+    let
+        todoTaskHtml =
+            List.map viewTodoTask todoTasks
+    in
+    div [] todoTaskHtml
+
+
+viewTodoTask : String -> Html Msg
+viewTodoTask task =
+    div []
+        [ text task
+        , button [ onClick (Complete task) ] [ text "Complete" ]
+        ]
+
+
+viewCompletedTasks : List String -> Html Msg
+viewCompletedTasks completedTasks =
+    let
+        completedTaskHtml =
+            List.map viewCompletedTask completedTasks
+    in
+    div [] completedTaskHtml
+
+
+viewCompletedTask : String -> Html Msg
+viewCompletedTask task =
+    div []
+        [ text task
+        , button [ onClick (Delete task) ] [ text "Remove" ]
+        ]
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ChangeStandupTask String
+    | Add
+    | Complete String
+    | Delete String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Add ->
+            addNewTask model.currentStandupTask model
+
+        Complete task ->
+            completeTask task model
+
+        Delete task ->
+            { model | completed = removeTask task model.completed }
+
+        ChangeStandupTask newInput ->
+            { model | currentStandupTask = newInput }
+
+
+addNewTask : String -> Model -> Model
+addNewTask newTask model =
+    { model | todo = model.currentStandupTask :: model.todo, currentStandupTask = "" }
+
+
+completeTask : String -> Model -> Model
+completeTask completedTask model =
+    { model
+        | todo = removeTask completedTask model.todo
+        , completed = completedTask :: model.completed
+    }
+
+
+removeTask : String -> List String -> List String
+removeTask taskToRemove tasks =
+    List.filter (\task -> task /= taskToRemove) tasks
